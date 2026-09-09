b476b66e9c77610a71df563c6941c709ac4e9b53
243495 <243495@KB37-1-303-C01>
2026-09-09 10:59:01 +0300

    Оформлен ввод услуг

--- ISIP224_Pokusaeva_Shalaevskiy/Program.cs
+++ ISIP224_Pokusaeva_Shalaevskiy/Program.cs
@@ -10,6 +10,18 @@ namespace ISIP224_Pokusaeva_Shalaevskiy
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество операций(от 2 до 40): ");
+            int col = Convert.ToInt32(Console.ReadLine());
+            if (col >= 2 && col <= 40)
+            {
+                for (int i = 1; i <= col; i++)
+                {
+                    Console.Write($"{i}.Введите название услуги или товара; количество денег(Руб.): ");
+                    string trata = (Console.ReadLine());
+                }
+            }
+            else Console.WriteLine("Введено неверное количество операций!");
+
         }
     }
 }
