99c26d798246c977d9d87e2745bb9b96c47687f6
User <User@DESKTOP-UEMS0SN>
2026-09-09 23:50:48 +0300

    Работа начата с начала. Оформлена функция запроса кол-ва операций.

--- ISIP224_Pokusaeva_Shalaevskiy/Program.cs
+++ ISIP224_Pokusaeva_Shalaevskiy/Program.cs
@@ -8,30 +8,31 @@ namespace ISIP224_Pokusaeva_Shalaevskiy
 {
     internal class Program
     {
-        void ToDict(in string[] items)
-        {
 
-        }
         static void Main(string[] args)
         {
-            Console.Write("Введите количество операций(от 2 до 40): ");
-            int col = Convert.ToInt32(Console.ReadLine());
-            string[] items = new string[col];
-            Dictionary<string, int> trata = new Dictionary<string, int>();
-            if (col >= 2 && col <= 40)
-            {
-                for (int i = 0; i <= col - 1; i++)
-                {
-                    Console.Write($"{i+1}.Введите название услуги или товара; количество денег(Руб.): ");
-                    items[i] = Console.ReadLine();
-                }
-                foreach (string j in items)
-                {
-                    Console.WriteLine(j);
-                }
-            }
-            else Console.WriteLine("Введено неверное количество операций!");
+
+            int operationCount = GetOperationCount();
 
         }
+        static int GetOperationCount()
+        {
+            int count;
+            while (true)
+            {
+                Console.Write("Введите количество операций (от 2 до 40): ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out count) && count >= 2 && count <= 40)
+                {
+                    return count;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Введите число от 2 до 40.");
+                }
+            }
+        }
     }
+    
 }
