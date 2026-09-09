be06aec25e386bdef5a78ca9606ac68f3e39b3b7
243495 <243495@KB37-1-303-C01>
2026-09-09 11:40:03 +0300

    Надо преобразовать ввод в словарь

--- ISIP224_Pokusaeva_Shalaevskiy/Program.cs
+++ ISIP224_Pokusaeva_Shalaevskiy/Program.cs
@@ -8,16 +8,26 @@ namespace ISIP224_Pokusaeva_Shalaevskiy
 {
     internal class Program
     {
+        void ToDict(in string[] items)
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.Write("Введите количество операций(от 2 до 40): ");
             int col = Convert.ToInt32(Console.ReadLine());
+            string[] items = new string[col];
+            Dictionary<string, int> trata = new Dictionary<string, int>();
             if (col >= 2 && col <= 40)
             {
-                for (int i = 1; i <= col; i++)
+                for (int i = 0; i <= col - 1; i++)
                 {
-                    Console.Write($"{i}.Введите название услуги или товара; количество денег(Руб.): ");
-                    string trata = (Console.ReadLine());
+                    Console.Write($"{i+1}.Введите название услуги или товара; количество денег(Руб.): ");
+                    items[i] = Console.ReadLine();
+                }
+                foreach (string j in items)
+                {
+                    Console.WriteLine(j);
                 }
             }
             else Console.WriteLine("Введено неверное количество операций!");
