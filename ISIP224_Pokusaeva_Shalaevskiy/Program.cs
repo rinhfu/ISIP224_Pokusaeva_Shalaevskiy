fd0b7e3734da6792fa31f0975a03090e61e70042
Андрей Шалаевский <mugaska2008@gmail.com>
2026-09-20 23:01:55 +0300

    Создан класс Product с параметрами.

--- ISIP224_Pokusaeva_Shalaevskiy/Program.cs
+++ ISIP224_Pokusaeva_Shalaevskiy/Program.cs
@@ -21,4 +21,36 @@ namespace ISIP224_Pokusaeva_Shalaevskiy
         Household = 4,
         Toys = 5
     }
+
+    public class Product
+    {
+        private string _name;
+        private decimal _price;
+        private int _quantity;
+
+        private static int _counter = 0;
+
+        public int Code { get; private set; }
+        public Category Category { get; private set; }
+
+        public string Name
+        {
+            get => _name;
+            private set => _name = value.Trim();
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            private set => _price = value;
+        }
+
+        public int Quantity
+        {
+            get => _quantity;
+            private set => _quantity = value;
+        }
+
+        public bool InStock => _quantity > 0;
+    }
 }
